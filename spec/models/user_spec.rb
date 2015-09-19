c6398eca2ae1b5a60ec1d769a44fd5ecfe468aaa
antonija_simic <antonija1023@gmail.com>
2015-09-19 17:02:27 +0200

    basic validations for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+ 
+ it "should be valid" do
+  user = User.new(name: "John", email: "john@mail.com")
+  expect(user).to be_valid
+ end
+
+ it "should have name" do
+  user = User.new(name: "", email: "john@mail.com")
+  expect(user).not_to be_valid
+ end
+
+ it "has name with max length of 50 char" do
+  user = User.new(name: "j", email: "john@mail.com")
+  user.name *= 51
+  expect(user).not_to be_valid
+  end
 end
