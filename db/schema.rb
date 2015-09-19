dfdd6dd6e758ff8f7e6f8ccecc8e3653e637e913
antonija_simic <antonija1023@gmail.com>
2015-09-19 16:06:50 +0200

    add users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919082407) do
+ActiveRecord::Schema.define(version: 20150919135548) do
 
   create_table "groceries", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
