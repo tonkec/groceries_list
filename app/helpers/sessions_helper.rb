98550c49ac9a0255a6161c2b3af5e968624deaac
antonija_simic <antonija1023@gmail.com>
2015-09-22 16:51:51 +0200

    add remember me function

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,15 +4,29 @@ module SessionsHelper
   end
   
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif cookies.signed[:user_id]
+      user = User.find_by(id: cookies.signed[:user_id])
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
-  def logged_in?
-    !current_user.nil?
-  end
+def logged_in?
+  !current_user.nil?
+end
 
-  def log_out
-   session.delete(:user_id)
-   @current_user = nil
- end
+def log_out
+ session.delete(:user_id)
+ @current_user = nil
+end
+
+def remember(user)
+  user.remember
+  cookies.permanent.signed[:user_id] = user.id
+  cookies.permanent[:remember_token] = user.remember_token
+end
 end
