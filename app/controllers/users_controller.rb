6a215913b3bd17f85a2f64baa6ac9dbc6a11a8c8
antonija_simic <antonija1023@gmail.com>
2015-09-22 16:59:21 +0200

    show method only for logged in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user , only: [:show]
   def new
     @user = User.new
 
@@ -25,5 +26,12 @@ class UsersController < ApplicationController
  def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in"
+      redirect_to login_path
     end
+  end
 end
