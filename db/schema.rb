e60f85282db3e27cae4756201922cd489ae7c0cb
antonija_simic <antonija1023@gmail.com>
2015-09-22 12:52:36 +0200

    add remember digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919151025) do
+ActiveRecord::Schema.define(version: 20150922105215) do
 
   create_table "groceries", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150919151025) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
