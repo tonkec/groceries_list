c7a6abe89ca623137897575f3796e540556aaa60
antonija_simic <antonija1023@gmail.com>
2015-09-23 12:26:27 +0200

    fix typo and add forget method

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,13 +21,17 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
-  def remember_me
+  def remember
     self.remember_token = User.new_token
-    update_attribute(remember_digest: User.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
