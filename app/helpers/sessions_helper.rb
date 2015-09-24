c9f488e34e0265cc8b675180d479fedd414e8029
antonija_simic <antonija1023@gmail.com>
2015-09-24 16:51:55 +0200

    forget user before deleting cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,18 +15,25 @@ module SessionsHelper
     end
   end
 
-def logged_in?
-  !current_user.nil?
-end
+  def logged_in?
+    !current_user.nil?
+  end
 
-def log_out
- session.delete(:user_id)
- @current_user = nil
-end
+  def log_out
+   forget(current_user)
+   session.delete(:user_id)
+   @current_user = nil
+  end
 
-def remember(user)
-  user.remember
-  cookies.permanent.signed[:user_id] = user.id
-  cookies.permanent[:remember_token] = user.remember_token
-end
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 end
