b89ada34960c12dc34b1a24b835340b82f262710
antonija_simic <antonija1023@gmail.com>
2015-09-25 14:34:53 +0200

    add remember and forget option

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user   
+      log_in user 
+      if params[:session][:remember_me ] == "1"
+        remember(user)
+      else 
+        forget(user)
+      end  
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
