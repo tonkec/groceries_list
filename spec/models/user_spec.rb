1ecfa659ff18f3cd94be14cda62b11f7a34a9cc3
antonija_simic <antonija1023@gmail.com>
2015-09-26 18:04:04 +0200

    better rspec logic; more before hooks, less repeated code;

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,64 +1,92 @@
 require 'rails_helper'
 
 describe User do
-  let!(:user) { FactoryGirl.create(:user) }
-  it "should be valid" do
-    expect(user).to be_valid
+  before {@user = User.new(name: "Antonija", email: "antonija@gmail.com",
+          password: "12345678", password_confirmation: "12345678")}
+  subject {@user}
+
+  it {should respond_to(:name)}
+  it {should respond_to(:email)}
+  it {should respond_to(:password_digest)}
+  it {should respond_to(:password_confirmation)}
+  it {should respond_to(:password)}
+  it {should respond_to(:authenticate)}
+
+  it {should be_valid}
+
+  describe "when name is not present" do
+    before {subject.name = ""}
+    it {should_not be_valid}
   end
 
-  it "should have name" do
-    user.name = ""
-    expect(user).not_to be_valid
+  describe "when email is not present" do
+    before {subject.email = ""}
+    it {should_not be_valid}
   end
 
-  it "has name with max length of 50 char" do
-    user.name = "j"
-    user.name *= 51
-    expect(user).not_to be_valid
+  describe "when password is not present" do
+    before {subject.password = " "}
+    it {should_not be_valid}
   end
 
-  it "should have mail" do
-    user.email = ""
-    expect(user).not_to be_valid
+  describe "when password is too short" do
+    before {subject.password = "a" * 7}
+    it {should_not be_valid}
   end
 
-  it "should have valid format of email" do
-
-   user.email = ""
-   valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-     first.last@foo.jp alice+bob@baz.cn]
-     valid_addresses.each do |valid_address|
-      user.email = valid_address
-      expect(user).to be_valid
+  describe "when password confirmation mismatch" do
+    before do
+      subject.password = "a"
+      subject.password_confirmation = "b"
     end
+
+    it {should_not be_valid}
   end
 
-  it "should not have invalid format of email" do
-   user.email = ""
-   invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+  describe "when email format is invalid" do
+   it "it is invalid" do
+     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
      foo@bar_baz.com foo@bar+baz.com]
      invalid_addresses.each do |invalid_address|
-      user.email = invalid_address
-      expect(user).not_to be_valid
+      subject.email = invalid_address
+      expect(subject).not_to be_valid
     end
+   end
   end
 
-  it "should have unique email" do
-    other_user = user.dup
-    other_user.save
-    other_user.email = user.email.upcase
-    expect(other_user).not_to be_valid
+  describe "when email format is valid" do
+   it "it is valid" do
+     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+     first.last@foo.jp alice+bob@baz.cn]
+     valid_addresses.each do |valid_address|
+      subject.email = valid_address
+      expect(subject).to be_valid
+    end
+   end
   end
 
-  #it "should not have blank password" do
-  #  user.password = ""
-   #  puts "#{user.password} weeee"
-   # expect(user).not_to be_valid
-#  end
+  describe "when email address is already taken" do
+  
+      before do
+        user_with_same_email = @user.dup
+        user_with_same_email.email = @user.email.upcase
+        user_with_same_email.save
+      end
 
-  it "should have password at least 8chr long" do
-    user.password = "a"
-    user.password *= 9
-    expect(user).not_to be_valid
+      it { should_not be_valid }
+   
+  end
+
+  describe "return value of authenticate" do
+    before {@user.save}
+    let (:found_user) {User.find_by_email(@user.email)}
+
+    describe "with valid psswd" do
+      it {should eq found_user.authenticate(@user.password)}
+    end
+
+    describe "with invalid psswd" do
+      it {should_not eq found_user.authenticate("wrong password")}
+    end
   end
 end
