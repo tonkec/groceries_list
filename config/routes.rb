18e2e676ed746f38befc0cedb1a20d970b7bf060
antonija_simic <antonija1023@gmail.com>
2015-09-27 15:32:46 +0200

    making home page root; add separate signup route; limit groceries only on several routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root "static_pages#home"
+  get 'signup' => 'users#new'
   get 'about'   => 'static_pages#about'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
-  resources :groceries
+  resources :groceries, only: [:new, :create, :edit, :update, :destroy]
   resources :users
 end
