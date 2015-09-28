b96d42a32705efa89fb057517bd8002f6d014f60
antonija_simic <antonija1023@gmail.com>
2015-09-28 12:10:24 +0200

    create instance variable groceries so it would be accessible within view and call paginate on it

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @user = User.new
+    @groceries = current_user.groceries.paginate(page: params[:page], :per_page => 4)
   end
 
   def about
