41a7e1b2ab64db361fd00f434a09afd986d54070
antonija_simic <antonija1023@gmail.com>
2015-09-28 12:14:35 +0200

    test respond to new attrbitues; test validation for user's id

--- spec/models/grocery_spec.rb
+++ spec/models/grocery_spec.rb
@@ -13,11 +13,24 @@ describe Grocery do
   it {should respond_to(:description)}
   it {should respond_to(:user_id)}
   it {should respond_to(:user)}
+  it {should respond_to(:amount)}
+  it {should respond_to(:price)}
+  it {should respond_to(:checked)}
 
   it {should be_valid}
 
-  it "is invalid without a title or description" do
-      @grocery.title = @grocery.description = ""
-      expect(subject).not_to be_valid
+  it "is invalid without a title" do
+    subject.title = ""
+    expect(subject).not_to be_valid
+  end 
+
+  it "is invalid without a user's id" do
+    subject.user_id = ""
+    expect(subject).not_to be_valid
+  end
+
+  it "has description with less than 250chr" do
+    subject.description = "a" * 251
+    expect(subject).not_to be_valid
   end 
 end
