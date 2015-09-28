0ecce81570d928fe2d7689995323203495b93097
antonija_simic <antonija1023@gmail.com>
2015-09-28 12:14:46 +0200

    validate presence of user's id

--- app/models/grocery.rb
+++ app/models/grocery.rb
@@ -1,6 +1,7 @@
 class Grocery < ActiveRecord::Base
   validates :title,  presence: true, length: { maximum: 50 }
-  validates :description,  presence: true, length: { maximum: 250 }
+  validates :description, length: { maximum: 250 }
+  validates :user_id, presence: true
   belongs_to :user
   default_scope -> { order('created_at DESC') }
 end
