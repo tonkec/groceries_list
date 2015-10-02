a16d4905d6b1e6151ebfa62cef89e4f1834577a9
antonija_simic <antonija1023@gmail.com>
2015-10-02 15:52:45 +0200

    change layout; navbar; unordered list for groceries; add dropdown list; add pagination; style pagination

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require materialize-sprockets
 //= require turbolinks
 //= require_tree .
+$(document).ready(function(){
+  $(".checkbox").click(function(){
+    var groceryTitle = $(this).siblings()[1];
+    $(groceryTitle).toggleClass("text-checked");
+  });
+});
