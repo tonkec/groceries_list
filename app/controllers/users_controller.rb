2d3eb994a6c1ac10dddc6cc51a27a0a384465215
antonija_simic <antonija1023@gmail.com>
2015-10-02 15:51:24 +0200

    redirect to root_url if logged_in; prevent showing signup page if logged_in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:show]
   def new
     @user = User.new
-
+    if logged_in? 
+      redirect_to root_url
+    end
   end
 
   def show
-    @user = User.find(params[:id])
-    @groceries = @user.groceries.paginate(page: params[:page], :per_page => 4)
-   
+    redirect_to root_url
   end
 
   def create 
